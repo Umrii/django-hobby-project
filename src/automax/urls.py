93b6d9584a76178d358a7aa193f091403232cccd
umrii <aatiq145@gmail.com>
2025-01-20 23:30:57 +0500

    Media Folder

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from main import urls as main_app_url
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(main_app_url)),
 ]
+
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
