afb48a6e49a5614c666b383e30ed716e49bf2532
umrii <aatiq145@gmail.com>
2025-01-21 23:48:05 +0500

    Django-Forms

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'localflavor',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'main',
     'users',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
