2932fad7998a0e7af7af005f2b9574b61919b204
umrii <aatiq145@gmail.com>
2025-06-18 05:51:55 +0500

    List View

--- src/main/views.py
+++ src/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .models import List
+
 def main_view(request):
     return render(request,'views/main.html',{"name":"AutoMax"})
 
@@ -15,3 +16,11 @@ def home_view(request):
         'listings': listings,
     }
     return render(request, "views/home.html", context)
+
+@login_required
+def list_view(request):
+    if request.method=='POST':
+        pass
+    elif request.method=='GET':
+        pass
+    return render(request, "views/main.html", {})
